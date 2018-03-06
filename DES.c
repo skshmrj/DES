acaecd29d93bc5d94ac1cbf894bbda578ce94709
saksham.raj (AutoChat) <saksham.raj@autochat.io>
2018-03-06 23:48:21 +0530

    Read message and key from file, whose name is supplied as command line argument

--- DES.c
+++ DES.c
@@ -1,20 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <ctype.h>
 #include "DES.h"
 #include "utilities.h"
 
-int main(){
+int main(int argc, char **argv){
+
+    printf("\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    printf("\nThis is an implementation of DES (Data Encryption Standard).");
+    printf("\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n");
+
     char *message = malloc(sizeof(char) * 1024);
     char *key = malloc(sizeof(char) * 8);
-    printf("\nThis is an implementation of DES (Data Encryption Standard).\n");
-    printf("\nEnter message to be encrypted.\n(Note: If left blank, input will be taken from default <input> file)\n");
-    // fgets(message, 1024, stdin);
-    gets(message);
-    char *res = string_to_binary(message);
-    printf("%ld\n", strlen(res));
-    puts(res);
-    // printf("\nEnter key.\n(Note: If left blank, default key will be selected from <key> file. A valid key length is 8 characters.)\n");
-    // fgets(key, 8, stdin);
+
+    /* Opening files for reading message and key. Filenames are supplied as the command line arguments 
+        with '-m' flag for file having the message and -k for file having the key */
+    int arg, prev_ind;
+    FILE *file;
+    while(prev_ind = optind, (arg = getopt(argc, argv, "m:k:")) != -1){
+        if ( optind == prev_ind + 2 && *optarg == '-' ) {
+            arg = ':';
+            -- optind;
+        }
+        switch (arg){
+            case 'm':
+                if (!(file = fopen(optarg, "r"))) {
+                    perror(optarg);
+                    abort ();
+                }
+
+                break;
+            case 'k':
+                if (!(file = fopen(optarg, "r"))) {
+                    perror(optarg);
+                    abort ();
+                }
+                break;
+            default:
+                abort ();
+        }
+    }
+
+    // char *res = string_to_binary(message);
     return 0;
 }
